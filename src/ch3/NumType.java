fece2a1d12bc1db08a4d3ecac7dd98a96b064352
alstj0801 <20210832@dongduk.ac.kr>
2024-04-09 00:03:57 +0900

    숫자형 연산

--- src/ch3/NumType.java
+++ src/ch3/NumType.java
@@ -1,12 +1,28 @@
 package ch3;
 
+
 public class NumType {
     public static void main(String[] args) {
+        // 숫자연산
         int a = 10;
         int b = 5;
         System.out.println(a + b);
         System.out.println(a - b);
         System.out.println(a * b);
         System.out.println(a / b);
+
+
+        // 증감연산
+        int i = 0;
+        int j = 10;
+        i++;
+        j--;
+
+        System.out.println(i);
+        System.out.println(j);
+
+        int k = 17;
+        System.out.println(k++);
+        System.out.println(k);
     }
 }
